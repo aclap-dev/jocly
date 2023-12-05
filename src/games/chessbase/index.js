2a9f0c1d9a0ad6f9cd481a8b266f0f6e3054863e
H.G.Muller <noreply@hccnet.nl>
2023-12-05 19:27:47 +0100

    New variant: Chu Shogi
    This implementation uses 3d kanji pieces from the tenjiku set, and fairy
    sprites for the 2d representation. It relies on fairy-move model for
    the double moves and anti-trading.

--- src/games/chessbase/index.js
+++ src/games/chessbase/index.js
@@ -7159,6 +7159,70 @@ exports.games = (function () {
 			"viewScripts": config_view_js_106
 		},
 		{
+			"name": "chu-shogi",
+			"modelScripts": modelScripts_108,
+			"config": {
+				"status": true,
+				"model": {
+					"title-en": "Chu Shogi",
+					"summary": "Historic 12x12 Shogi variant",
+					"rules": {
+						"en": "shogi/chu-shogi-rules.html"
+					},
+					"module": "chessbase",
+					"plazza": "true",
+					"thumbnail": "res/rules/shogi/chu-shogi-thumb.png",
+					"released": 1396536978,
+					"credits": {
+						"en": "shogi/shogi-credits.html"
+					},
+					"gameOptions": config_model_gameOptions_2,
+					"js": modelScripts_108,
+					"description": {
+						"en": "shogi/chu-shogi-description.html"
+					},
+					"levels": config_model_levels_15
+				},
+				"view": {
+					"title-en": "Chessbase view",
+					"visuals": {
+						"600x600": [
+							"res/visuals/tori-600x600-3d.jpg",
+							"res/visuals/tori-600x600-2d.jpg"
+						]
+					},
+					"xdView": true,
+					"css": config_view_css,
+					"preferredRatio": 1,
+					"useShowMoves": true,
+					"useNotation": true,
+					"module": "chessbase",
+					"defaultOptions": config_view_defaultOptions,
+					"skins": [
+						{
+							"name": "skin3d",
+							"title": "3D Classic",
+							"3d": true,
+							"preload": [
+								"smoothedfilegeo|0|/res/ring-target.js",
+								"image|/res/images/cancel.png",
+								"image|/res/images/wikipedia.png"
+							],
+							"world": config_view_skins_world,
+							"camera": config_view_skins_camera
+						},
+						config_view_skins_2
+					],
+					"animateSelfMoves": false,
+					"switchable": true,
+					"sounds": config_view_sounds,
+					"js": config_view_js_108,
+					"useAutoComplete": true
+				}
+			},
+			"viewScripts": config_view_js_108
+		},
+		{
 			"name": "makromachy",
 			"modelScripts": modelScripts_109,
 			"config": {
