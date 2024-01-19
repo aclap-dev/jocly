bf7510dd02522c93699a48fd012245c9948bfb65
H.G.Muller <noreply@hccnet.nl>
2024-01-19 22:07:17 +0100

    Scirocco: rely on default cbMoveMidZ

--- src/games/chessbase/decimal/scirocco-view.js
+++ src/games/chessbase/decimal/scirocco-view.js
@@ -40,38 +40,15 @@
 			pieces: this.cbFairyPieceStyle({
 				"default": {
 					"2d": {
-						width: 1050,
-						height: 1050,						
+						width: 945,
+						height: 945,						
 					},
 					"3d": {
-						scale: [.5,.5,.5],
+						scale: [.45,.45,.45],
 					},
 				},
 			}),
 		};
-	}
-
-	View.Board.cbMoveMidZ = function(aGame,aMove,zFrom,zTo) {
-		var d = aMove.t - aMove.f, jump = 0;
-		if(aMove.a=='N' || aMove.a=='A' || aMove.a=='D' || aMove.a=='C' ||
-		   aMove.a=='Dv' || aMove.a=='Sq' || aMove.a=='Wi' || aMove.a=='Z') jump = 1; 		else {
-			var dist = (d>11 || d<-11 || d>1 && d<9 || d<-1 && d>-9);
-	  	if(dist) {
-				if(aMove.a=='S' || aMove.a=='G' || aMove.a=='Ma' || aMove.a=='Pr' ||
-				   aMove.a=='T' || aMove.a=='Zi' || aMove.a=='Za' || aMove.a=='L' || aMove.a=='E')
-					jump = 1;
-				else if(aMove.a=='Ab' || aMove.a=='Dk') {
-					var diag = 0, orth = 0;
-					for(var i=-9; i<9; i++) {
-						if(d == 9*i || d == 11*i) diag = 1;
-						if(d == 1*i || d == 10*i) orth = 1;
-					}
-		  		jump = (aMove.a=='Ab' ? !diag : !orth);
-		}	}	}
-		if(jump) 
-			return Math.max(zFrom,zTo)+1500;
-		else
-			return (zFrom+zTo)/2 - 0.001*(aMove.a == 'O');
 	}
 
 })();
