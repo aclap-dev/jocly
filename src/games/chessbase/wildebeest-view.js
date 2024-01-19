78177124af20b0f4a306b0a0911dd3a5cc9f86e6
H.G.Muller <noreply@hccnet.nl>
2024-01-19 20:44:49 +0100

    Wildebeest Chess: fix view
    The animation wrapper to do the extra castlings is also no longer needed.
    Wjile we are at it, the cbMoveMidZ was also removed, as the default
    routine now does exactly the same thing.

--- src/games/chessbase/wildebeest-view.js
+++ src/games/chessbase/wildebeest-view.js
@@ -25,7 +25,7 @@
 				"3d": this.cbGridBoard.coordsFn.call(this,wildebeestBoard3d),
 			},
 			boardLayout: [
-	      	"#.#.#.#.#.#",
+			"#.#.#.#.#.#",
 	      		".#.#.#.#.#.",
 	     		"#.#.#.#.#.#",
 	      		".#.#.#.#.#.",
@@ -34,7 +34,7 @@
 	     		"#.#.#.#.#.#",
 	      		".#.#.#.#.#.",
 	     		"#.#.#.#.#.#",
-				".#.#.#.#.#.",
+			".#.#.#.#.#.",
 			],
 			board: {
 				"2d": {
@@ -61,48 +61,6 @@
 				},
 			}),
 		};
-	}
-
-	/* Make the knight, camel and wildebeest jump when leaping */
-	View.Board.cbMoveMidZ = function(aGame,aMove,zFrom,zTo) {
-		var geometry = aGame.cbVar.geometry;
-		var x0 = geometry.C(aMove.f);
-		var x1 = geometry.C(aMove.t);
-		var y0 = geometry.R(aMove.f);
-		var y1 = geometry.R(aMove.t);
-		if(x1-x0==0 || y1-y0==0 || Math.abs(x1-x0)==Math.abs(y1-y0))
-			return (zFrom+zTo)/2;
-		else
-			return Math.max(zFrom,zTo)+1500;
-	}
-
-	var SuperViewBoardcbAnimate = View.Board.cbAnimate;
-	View.Board.cbAnimate = function(xdv,aGame,aMove,callback) {
-
-		var piece=this.pieces[this.board[aMove.f]];
-		if(piece.t==8 && !this.castled[this.mWho] && aMove.cg===undefined) {
-			var geometry = aGame.cbVar.geometry;
-			var dc=Math.abs(geometry.C(aMove.t)-geometry.C(aMove.f));
-			if(dc>=2) {
-				var extra=aGame.wbExtraCastleRook[aMove.t];
-				var rPiece=this.pieces[this.board[extra.r0]];
-
-				var animCount=2;
-				function EndAnim() {
-					if(--animCount==0)
-						callback();
-				}
-				SuperViewBoardcbAnimate.call(this,xdv,aGame,aMove,function() {
-					EndAnim();
-				});
-				var displaySpec=aGame.cbMakeDisplaySpecForPiece(aGame,extra.r,rPiece);
-				xdv.updateGadget("piece#"+rPiece.i,displaySpec,600,function() {
-					EndAnim();
-				});
-				return;
-			}
-		}
-		SuperViewBoardcbAnimate.apply(this,arguments);
 	}
 
 })();
